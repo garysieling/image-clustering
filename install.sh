15bb66e8ba82fdca614c1dfa81569d762f2171d5
Gary Sieling <gary@garysieling.com>
2019-04-26 22:08:34 -0400

    libraries

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+pip3 install tensorflow
+pip3 install facenet
+pip3 install pandas
+pip3 install nltk
+pip3 install gensim
+
+# not sure about that one
+#pip3 install align
+
 pip3 install pybind11 numpy setuptools
 git clone https://github.com/nmslib/hnswlib.git
 cd hnswlib/python_bindings
